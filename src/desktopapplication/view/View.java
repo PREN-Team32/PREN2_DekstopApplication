757bed4a20defcf8f4eb9ea97cd44fdb229b24ca
hizonomi <niklausmanser@hotmail.com>
2015-02-27 09:02:47 +0100

    Packages created

--- src/desktopapplication/view/View.java
+++ src/desktopapplication/view/View.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pren2_desktopapplication;
+package desktopapplication.view;
 
 /**
  *
  * @author Niklaus
  */
-public class PREN2_DesktopApplication {
+public class View {
 
     /**
      * @param args the command line arguments
