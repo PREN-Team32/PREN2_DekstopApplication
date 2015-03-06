690ee2c11fc90003cf23b22324a9e4cffd6f63c0
Nik <niklausmanser@hotmail.com>
2015-03-06 10:30:43 +0100

    Package names edited

--- src/ch/hslu/pren/t32/desktopapplication/control/BluetoothConnection.java
+++ src/ch/hslu/pren/t32/desktopapplication/control/BluetoothConnection.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desktopapplication.control;
+package ch.hslu.pren.t32.desktopapplication.control;
 
 /**
  *
