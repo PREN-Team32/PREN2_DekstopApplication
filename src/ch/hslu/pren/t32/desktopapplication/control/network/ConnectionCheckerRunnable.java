fb399ab1c95423120e9bb6b27c8687d1909c4833
hizonomi <niklausmanser@hotmail.com>
2015-05-08 10:25:02 +0200

    ConnectionChecker: closing socket in finally block, converted to try-with-resources

--- src/ch/hslu/pren/t32/desktopapplication/control/network/ConnectionCheckerRunnable.java
+++ src/ch/hslu/pren/t32/desktopapplication/control/network/ConnectionCheckerRunnable.java
@@ -9,9 +9,6 @@ import ch.pren.model.ValueItem;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
-import java.net.SocketException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -42,32 +39,21 @@ public class ConnectionCheckerRunnable implements Runnable {
     
     @Override
     public void run() {
-        try {
-            while(true) {
-                System.out.println("#ConnectionChecker: Starting to check for incoming ValueItems.");
-                Socket clientSocket = new Socket(hostIP, 11111);
+        while(true) {
+            System.out.println("#ConnectionChecker: Starting to check for incoming ValueItems.");
+            try (Socket clientSocket = new Socket(hostIP, 11111)) {
+                ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+                ValueItem tmp = (ValueItem) ois.readObject();
+                System.out.println("#ConnectionChecker: New Values received.");
+                newValues.overrideValues(tmp);
+            }
+            catch(IOException | ClassNotFoundException e) {
                 try {
-                    ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
-                    ValueItem tmp = (ValueItem) ois.readObject();
-                    System.out.println("#ConnectionChecker: New Values received.");
-                    newValues.overrideValues(tmp);
-                }
-                catch(Exception e) {
-                    try {
-                        Thread.sleep(100);
-                        e.printStackTrace();
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                
-
-                clientSocket.close();
+                    Thread.sleep(100);
+                    System.err.println("#ConnectionChecker: " + e.getMessage());
+                } catch (InterruptedException ex) {}
             }
         }
-        catch(IOException ex) {
-            ex.printStackTrace();
-        }
-
+        
     }
 }
