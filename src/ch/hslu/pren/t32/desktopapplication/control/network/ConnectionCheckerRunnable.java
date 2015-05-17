4464333a501fca995ca24a287722bc6a1a094f9a
LivioKunz <livio_kunz@hotmail.com>
2015-05-17 16:12:01 +0200

    ConfigItem geupdated mit UUID

--- src/ch/hslu/pren/t32/desktopapplication/control/network/ConnectionCheckerRunnable.java
+++ src/ch/hslu/pren/t32/desktopapplication/control/network/ConnectionCheckerRunnable.java
@@ -6,9 +6,18 @@
 package ch.hslu.pren.t32.desktopapplication.control.network;
 
 import ch.pren.model.ValueItem;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import sun.misc.BASE64Decoder;
 
 /**
  *
@@ -45,7 +54,9 @@ public class ConnectionCheckerRunnable implements Runnable {
                 ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
                 ValueItem tmp = (ValueItem) ois.readObject();
                 System.out.println("#ConnectionChecker: New Values received.");
-                newValues.overrideValues(tmp);
+                newValues.overrideValues(tmp);       
+                displayImage(Base64toImage(newValues.editedImage));
+                break;
             }
             catch(IOException | ClassNotFoundException e) {
                 try {
@@ -53,7 +64,35 @@ public class ConnectionCheckerRunnable implements Runnable {
                     System.err.println("#ConnectionChecker: " + e.getMessage());
                 } catch (InterruptedException ex) {}
             }
+        }        
+    }
+    
+         public void displayImage(Image img2) {
+        //BufferedImage img=ImageIO.read(new File("/HelloOpenCV/lena.png"));
+        ImageIcon icon = new ImageIcon(img2);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(img2.getWidth(null) + 50, img2.getHeight(null) + 50);
+        JLabel lbl = new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+       
+    public BufferedImage Base64toImage(String imageString){
+          BufferedImage image = null;
+        byte[] imageByte;
+        try {
+            BASE64Decoder decoder = new BASE64Decoder();
+            imageByte = decoder.decodeBuffer(imageString);
+            ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+            image = ImageIO.read(bis);
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
+        return image;
     }
 }
